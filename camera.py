2d38d5d395734c97947b910f9980caaae6497803
Pablo Rivera Conde <pabloriveraconde@gmail.com>
2014-08-23 14:48:37 +0000

    Actually taking photos

--- camera.py
+++ camera.py
@@ -1,7 +1,15 @@
+import time; import picamera;
+
 class Camera:
-  def hello(self):
-    return 'Hello';
-  
-  
-##MAIN
-print(Camera().hello());
+
+  def get_rand_filename(self):
+    return "/home/pi/photo_" + time.strftime("%d_%H:%M:%s") + ".jpg";
+
+  def take_photo(self):
+    print("Taking photo");
+    with picamera.PiCamera() as camera:
+      time.sleep(5);
+      camera.capture(self.get_rand_filename());
+      camera.stop_preview();
+    print("Done");
+
